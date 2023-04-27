df1b19a12dadd4a220525ffab0bcb5e0481bf603
Simone Callegari <87664284+SCA-ZMT@users.noreply.github.com>
2023-04-27 11:00:51 +0200

    bump version for ease of installation (#6)

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ INSTALL_REQUIREMENTS = list( read_reqs(CURRENT_DIR / "requirements.txt") )
 
 SETUP = dict(
     name="iec62209",
-    version="1.0.0",
+    version="1.0.1",
     description="Publication-IEC62209 package",
     author=", ".join(
         (
